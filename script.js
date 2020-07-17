789ad76fd7769cb8a0d0adbdec2fc3ed9c72fa80
nkleinmann <nekleinmann@gmail.com>
2020-07-16 21:16:20 -0400

    Basic JS and skeleton for rest of HTML

---
+++ script.js
@@ -0,0 +1,10 @@
+// makes sure everything is loaded before running JavaScript
+$(document).ready(function() {
+    // need to declare timer
+    // need to declare score
+
+    $("#button-start").on("click", function() {
+        console.log("You clicked a button!");
+
+    })
+})
