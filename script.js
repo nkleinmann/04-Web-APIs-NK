5207ace119b2030e4b447df8c25aa7f13573d476
nkleinmann <nekleinmann@gmail.com>
2020-07-16 22:00:29 -0400

    Start button changes to question page

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
 
     $("#button-start").on("click", function () {
         console.log("You clicked a button!");
-        
+        //Resource: https://www.tutorialrepublic.com/faq/how-to-change-css-display-property-to-none-or-block-using-jquery.php#:~:text=Answer%3A%20Use%20the%20jQuery%20css,to%20the%20elements%20i.e.%20inline.
+        $("#startPage").css("display", "none");
+        $("#questionPage").css("display", "block");
     })
 })
